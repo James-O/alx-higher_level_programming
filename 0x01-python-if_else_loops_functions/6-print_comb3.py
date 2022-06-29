94ea5ea1950d97f2b8930480c4763114937e0ac6
James Ogbonna <ogbonnasunday42@gmail.com>
2022-06-28 17:54:16 -0700

    corrections

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) > 96:
-        return True
-    return False
+number = 0
+while number <= 89:
+    if number % 10 == 0:
+        number += 1 + number // 10
+    print("{:02d}".format(number), end='\n' if number == 89 else ", ")
+    number += 1
